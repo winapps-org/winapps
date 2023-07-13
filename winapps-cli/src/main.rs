2866a1d1b235ee160595f5899fa1b655786c6bea
LDprg <lukas_4dr@gmx.at>
2023-07-13 20:02:57 +0200

    Add basic cli and config file parsing

--- winapps-cli/src/main.rs
+++ winapps-cli/src/main.rs
@@ -1,3 +1,33 @@
+use clap::Command;
+
+fn cli() -> Command {
+    Command::new("winapps-cli")
+        .about("The winapps-cli is a command line interface for winapps")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(Command::new("check").about("Checks remote connection"))
+        .subcommand(Command::new("connect").about("Connects to remote"))
+}
+
 fn main() {
-    println!("Test lib: {}", winapps::add(1, 2));
+    let cli = cli();
+    let matches = cli.clone().get_matches();
+
+    match matches.subcommand() {
+        Some(("check", _)) => {
+            println!("Checking remote connection");
+
+            let _config = winapps::load_config(None);
+        }
+        Some(("connect", _)) => {
+            println!("Connecting to remote");
+        }
+        Some((_, _)) => {
+            cli.about("Command not found try existing ones!")
+                .print_help()
+                .unwrap();
+        }
+        _ => unreachable!(),
+    }
 }
