e13b9e24a217c5d2131756fea6b4ab0a8b100e7f
Rohan Barar <rohan.barar@gmail.com>
2024-07-16 23:22:15 +1000

    Updated bin/winapps to support FreeRDP Flatpak. [Addendum] Attempted 'pre-commit install && pre-commit run --all-files' to attempt to fix issues with pre-commit.

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2034              # Silence warnings regarding unused variables globally.
+# shellcheck disable=SC2034           # Silence warnings regarding unused variables globally.
 
 ### GLOBAL CONSTANTS ###
 # ANSI ESCAPE SEQUENCES
