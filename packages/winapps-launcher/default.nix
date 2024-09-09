b044cb41c7eb1b6d6240d00007eee9acb82cf57f
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-09-09 16:12:22 +0200

    fix: properly name patches & remove nixfmt pre-commit hook since it requires GHC & hackage

--- packages/winapps-launcher/default.nix
+++ packages/winapps-launcher/default.nix
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
     (callPackage ../winapps { })
   ];
 
-  patches = [ ./WinAppsLauncher.diff ];
+  patches = [ ./WinAppsLauncher.patch ];
 
   postPatch = ''
     substituteAllInPlace WinAppsLauncher.sh
