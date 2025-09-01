cdb5c3ef7f5d57f991bb3988a9870a0d436c1c1a
eylenburg <84839316+eylenburg@users.noreply.github.com>
2025-09-01 11:16:17 +0100

    Update TimeSync.ps1
    Signed-off-by: Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>

--- oem/TimeSync.ps1
+++ oem/TimeSync.ps1
@@ -1,7 +1,7 @@
 # Script to monitor if there is a sleep_marker created by WinApps (indicating the Linux host was suspended) in order to trigger a time sync as the time in the Windows VM will otherwise drift while Linux is suspended.
 
 # Define the path to monitor. Make sure this matches the location for the sleep_marker in the Winapps script (need to match the APPDATA path).
-$filePath = "\\tsclient\home\.local\share\linoffice\sleep_marker"
+$filePath = "\\tsclient\home\.local\share\winapps\sleep_marker"
 $networkPath = "\\tsclient\home"
 
 # Function to check and handle file
